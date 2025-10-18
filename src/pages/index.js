e5abc7d810b6d2419f4c8671cce416e17197c4c2
Dan Hostetler <dan.j.hostetler@gmail.com>
2025-10-18 09:49:46 -0500

    Verbiage change

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ return (
 			<div className='main-page'>
 				<h3>Welcome to my portfolio page. </h3>
 				<p>It gives details about who I am through testimonials and self reflections and a full detailing of my career and past work history with employers and clients.</p>
-				<p>There are many UI components on the pages so be sure to play around and try out the Day/Night style functionality and tap and click to see more information on clients and timeline events.</p>
+				<p>There are many UI components on the pages so be sure to play around and try out the Day/Night style functionality and click or tap to see more information on clients and timeline events.</p>
 				<p>Thank you for stopping by.</p>
 			</div>
 		</div>
