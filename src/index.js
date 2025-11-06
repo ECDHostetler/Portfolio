d41af952f6dd582b69bcffda68dbd1cf95bba6a1
Dan J Hostetler <33108666+ECDHostetler@users.noreply.github.com>
2025-11-05 18:45:31 -0600

    Moving to installed Bootstrap instead of CDN hosted.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'rsuite/Timeline/styles/index.css';
